ad4033932c9910c9d6e832c0c9a2a03cbc30d141
Artem Lodygin <aldaer@yandex.ru>
2017-02-01 04:07:22 +0300

    Fiddling with styles

--- src/main/resources/WEB-INF/extras/main.js
+++ src/main/resources/WEB-INF/extras/main.js
@@ -1,9 +1,19 @@
+const NUMPAD = $(".setnumber");
+
 function cellClick(el) {
     $('td').removeClass("selected");
     cellid = el.getAttribute("id");
     $('#coords').text(cellid);
     $('input[name="cell"]').val(cellid);
     $(el).addClass("selected");
+    $(el).append(NUMPAD);
+    NUMPAD.removeClass("hidden");
+    NUMPAD.css("display", "inherit");
+}
+
+function hidePad() {
+    alert("Hiding");
+    NUMPAD.addClass("hidden");
 }
 
 if ($('.hint').length) $('input[name="hint"]').prop("checked", true);
